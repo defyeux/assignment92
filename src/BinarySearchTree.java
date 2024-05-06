0405c1f5f7b0a98deb29f2bc879d5120e9a810f1
unknown <230487@astanait.edu.kz>
2024-05-06 12:31:06 +0500

    yeux

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,14 +1,15 @@
 public class BinarySearchTree<K extends Comparable<K>, V> {
-    Node root;
+    private Node root;
 
-    class Node {
-        K key;
-        V value;
-        Node left, right;
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
 
         public Node(K key, V value) {
             this.key = key;
             this.value = value;
+            left = right = null;
         }
 
     }
@@ -49,14 +50,76 @@ public class BinarySearchTree<K extends Comparable<K>, V> {
         }
     }
 
+    public void inOrder() {
+        inOrder(root);
+        System.out.println();
+    }
+
+    private void inOrder(Node node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.print(node.value + " ");
+            inOrder(node.right);
+        }
+    }
+
 
     public V get(K key) {
-        return null;
+        return get(root, key);
+    }
+
+    private V get(Node node, K key) {
+        int n = key.compareTo(node.key);
+        if (node == null) {
+            return null;
+        }
+        if (n > 0) {
+            return get(node.left, key);
+        }
+        if (n < 0) {
+            return get(node.right, key);
+        }
+
+        return node.value;
     }
 
     public void delete(K key) {
-
+        root = delete(root, key);
     }
+
+    private Node delete(Node node, K key) {
+        int n = key.compareTo(node.key);
+        if (node == null) {
+            return null;
+        }
+        if (n > 0) {
+             node.left = delete(node.left, key);
+        } else if (n < 0) {
+            node.right = delete(node.right, key);
+        } else if (n == 0) {
+            if (node.left == null && node.right == null) {
+                return null;
+            }
+            if (node.left == null) {
+                return node.right;
+            }
+            if (node.right == null) {
+                return node.left;
+            }
+
+            Node smallestValue = findSmallestValue(node.left);
+            node.key = smallestValue.key;
+            node.value = smallestValue.value;
+            node.left = delete(node.right, key);
+         }
+
+        return node;
+    }
+
+    private Node findSmallestValue(Node node) {
+        return node.right == null ? root: findSmallestValue(node.right);
+    }
+
 
     public Iterable<K> iterator() {
         return null;
--- src/TestForBST.java
+++ src/TestForBST.java
@@ -1,11 +1,10 @@
 public class TestForBST {
     public static void main(String[] args) {
         BinarySearchTree<Integer, Integer> tree = new BinarySearchTree();
-        tree.put(3, 3);
+        tree.put(3, 4);
         tree.put(4, 4);
         tree.put(5, 5);
-        System.out.println(tree.root.key);
-        System.out.println(tree.root.right.key);
-        System.out.println(tree.root.right.right.key);
+        tree.inOrder();
+        System.out.println(tree.get(3));
     }
 }
